0ab2a5140e9ee6953a2327bb52f161c3ff059344
M1KADO <l2.astreia@gmail.com>
2021-08-31 22:12:50 -0700

    Testing Forked Repo Push

--- src/main/java/com/project/nyvia380/Nyvia380Application.java
+++ src/main/java/com/project/nyvia380/Nyvia380Application.java
@@ -9,9 +9,9 @@ public class Nyvia380Application {
     public static void main(String[] args) {
         SpringApplication.run(Nyvia380Application.class, args);
 
-
         TestClass.print();
         TestClass.printThis("Testing Git Collaborator Privilege.");
+        TestClass.printThis("Testing Forked Repo Push.");
     }
 
 }
