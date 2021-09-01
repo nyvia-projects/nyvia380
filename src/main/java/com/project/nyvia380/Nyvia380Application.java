45bd2c8967771d00ebdbdb5401853a7c668579fb
Nyvia Projects <nyvia.projects@gmail.com>
2021-08-31 19:54:49 -0700

    Setup/Configure

---
+++ src/main/java/com/project/nyvia380/Nyvia380Application.java
@@ -0,0 +1,16 @@
+package com.project.nyvia380;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nyvia380Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nyvia380Application.class, args);
+
+
+
+    }
+
+}
