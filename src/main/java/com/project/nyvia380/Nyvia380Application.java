520bb8c6c0a9fd196e9d198e82015080543d0613
Nyvia Projects <nyvia.projects@gmail.com>
2021-09-01 22:41:05 -0700

    Updated Home Page UI Created LaunchBrowserTo() method

--- src/main/java/com/project/nyvia380/Nyvia380Application.java
+++ src/main/java/com/project/nyvia380/Nyvia380Application.java
@@ -1,17 +1,53 @@
 package com.project.nyvia380;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class Nyvia380Application {
 
     public static void main(String[] args) {
-        System.setProperty("server.port", "8080");
         SpringApplication.run(Nyvia380Application.class, args);
+        /*
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/static/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:8080/");*/
 
-
+        LaunchBrowserTo("http://localhost:8080/");
 
     }
+/*
+    @EventListener({ApplicationReadyEvent.class})
+    void applicationReadyEvent() {
+        System.out.println("Application started ... launching browser now");
+        LaunchBrowserTo("http://localhost:8080/");
+    }*/
 
+    public static void LaunchBrowserTo(String url) {
+        if(Desktop.isDesktopSupported()){
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
